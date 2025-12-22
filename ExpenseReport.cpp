ccb29b99c6be16051b2d32d7bc96fc2c64460c56
Lucas Piorno <lucasp01012002@gmail.com>
2025-12-22 23:40:38 +0100

    first commit

---
+++ ExpenseReport.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include "ExpenseReport.h"
+
+using namespace std;
+
+void printReport(list<Expense> expenses)
+{
+    int total = 0;
+    int mealExpenses = 0;
+
+    auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    cout << "Expenses " << ctime(&now);
+
+    for (list<Expense>::iterator expense = expenses.begin(); expense != expenses.end(); ++expense) {
+        if (expense->type == BREAKFAST || expense->type == DINNER) {
+            mealExpenses += expense->amount;
+        }
+
+        string expenseName = "";
+        switch (expense->type) {
+        case DINNER:
+            expenseName = "Dinner";
+            break;
+        case BREAKFAST:
+            expenseName = "Breakfast";
+            break;
+        case CAR_RENTAL:
+            expenseName = "Car Rental";
+            break;
+        }
+
+        string mealOverExpensesMarker = (expense->type == DINNER && expense->amount > 5000) || (expense->type == BREAKFAST && expense->amount > 1000) ? "X" : " ";
+
+        cout << expenseName << '\t' << expense->amount << '\t' << mealOverExpensesMarker << '\n';
+
+        total += expense->amount;
+    }
+
+    cout << "Meal expenses: " << mealExpenses << '\n';
+    cout << "Total expenses: " << total << '\n';
+}
